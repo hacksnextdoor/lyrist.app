de843029f41e6aeba8fef6a81613cd76f3ca869f
Peyt Spencer Dewar <psdewar2@live.com>
2025-06-11 18:34:18 -0700

    Updated reviews

--- components/Reviews.js
+++ components/Reviews.js
@@ -90,7 +90,7 @@ export function Reviews() {
             <LyristText
               style={[styles.paginationText, {fontSize: small ? 24 : medium ? 32 : 40}]}
               weight={'Medium'}>
-              Thousands love their{' '}
+              The people love their{' '}
               <Link role="link" href={'#features'} style={{textDecoration: 'none'}}>
                 <Pressable onHoverIn={() => setLine0(true)} onHoverOut={() => setLine0(false)}>
                   <LyristText
@@ -116,73 +116,70 @@ export function Reviews() {
             />
           </Pressable>
         </View>
-        <View style={[styles.card, styles.cardShadow, (small || medium) && {marginBottom: 32}]}>
-          <View style={styles.stars}>
-            {Array.from(Array(stars), (_, k) => (
-              <FaStar
-                key={k}
-                color="#FEBB43"
-                style={{marginRight: 2}}
-                size={small ? 16 : medium ? 24 : 32}
-              />
-            ))}
-            {Array.from(Array(5 - stars), (_, k) => (
-              <FaRegStar
-                key={k}
-                color="#FEBB43"
-                style={{marginRight: 2}}
-                size={small ? 16 : medium ? 24 : 32}
-              />
-            ))}
-          </View>
-          <ScrollView
-            key={currentIndex}
-            style={styles.reviewScroll} // maxHeight not strictly needed since parent is fixed
-            showsVerticalScrollIndicator={false}>
+        <View style={[medium && {minHeight: 406}, small && {minHeight: 320}]}>
+          <View style={[styles.card, styles.cardShadow]}>
+            <View style={styles.stars}>
+              {Array.from(Array(stars), (_, k) => (
+                <FaStar
+                  key={k}
+                  color="#FEBB43"
+                  style={{marginRight: 2}}
+                  size={small ? 16 : medium ? 24 : 32}
+                />
+              ))}
+              {Array.from(Array(5 - stars), (_, k) => (
+                <FaRegStar
+                  key={k}
+                  color="#FEBB43"
+                  style={{marginRight: 2}}
+                  size={small ? 16 : medium ? 24 : 32}
+                />
+              ))}
+            </View>
             <LyristText style={{fontSize: small ? 16 : medium ? 24 : 32, fontStyle: 'italic'}}>
               {content}
             </LyristText>
-          </ScrollView>
-          <View style={{flexDirection: 'row', alignItems: 'center', gap: 12}}>
-            <LyristText style={{fontSize: small ? 16 : medium ? 24 : 32}} weight={'Medium'}>
-              {name}
-            </LyristText>
-            {msRef.current && (
-              <View
-                style={{
-                  width: ICON_SIZE,
-                  height: ICON_SIZE,
-                  borderRadius: ICON_SIZE / 2,
-                  borderWidth: small ? 3 : medium ? 4 : 6,
-                  borderTopColor: x <= 0.75 * ms ? 'transparent' : LYRIST_BLUE,
-                  borderRightColor:
-                    x <= 0.5 * ms
-                      ? 'transparent'
-                      : x <= 0.75 * ms
-                      ? `${LYRIST_BLUE}CC`
-                      : LYRIST_BLUE,
-                  borderBottomColor:
-                    x <= 0.25 * ms
-                      ? 'transparent'
-                      : x <= 0.5 * ms
-                      ? `${LYRIST_BLUE}80`
-                      : x <= 0.75 * ms
-                      ? `${LYRIST_BLUE}CC`
-                      : LYRIST_BLUE,
-                  borderLeftColor:
-                    x <= 0
-                      ? 'transparent'
-                      : x <= 0.25 * ms
-                      ? `${LYRIST_BLUE}40`
-                      : x <= 0.5 * ms
-                      ? `${LYRIST_BLUE}80`
-                      : x <= 0.75 * ms
-                      ? `${LYRIST_BLUE}CC`
-                      : LYRIST_BLUE,
-                  transform: 'rotate(45deg)',
-                }}
-              />
-            )}
+            <View style={{flexDirection: 'row', alignItems: 'center', gap: 12}}>
+              <LyristText style={{fontSize: small ? 16 : medium ? 24 : 32}} weight={'Medium'}>
+                {name}
+              </LyristText>
+              {msRef.current && (
+                <View
+                  style={{
+                    width: ICON_SIZE,
+                    height: ICON_SIZE,
+                    borderRadius: ICON_SIZE / 2,
+                    borderWidth: small ? 3 : medium ? 4 : 6,
+                    borderTopColor: x <= 0.75 * ms ? 'transparent' : LYRIST_BLUE,
+                    borderRightColor:
+                      x <= 0.5 * ms
+                        ? 'transparent'
+                        : x <= 0.75 * ms
+                        ? `${LYRIST_BLUE}CC`
+                        : LYRIST_BLUE,
+                    borderBottomColor:
+                      x <= 0.25 * ms
+                        ? 'transparent'
+                        : x <= 0.5 * ms
+                        ? `${LYRIST_BLUE}80`
+                        : x <= 0.75 * ms
+                        ? `${LYRIST_BLUE}CC`
+                        : LYRIST_BLUE,
+                    borderLeftColor:
+                      x <= 0
+                        ? 'transparent'
+                        : x <= 0.25 * ms
+                        ? `${LYRIST_BLUE}40`
+                        : x <= 0.5 * ms
+                        ? `${LYRIST_BLUE}80`
+                        : x <= 0.75 * ms
+                        ? `${LYRIST_BLUE}CC`
+                        : LYRIST_BLUE,
+                    transform: 'rotate(45deg)',
+                  }}
+                />
+              )}
+            </View>
           </View>
         </View>
       </View>
@@ -195,7 +192,6 @@ const styles = StyleSheet.create({
     gap: 16,
   },
   card: {
-    flexGrow: 1,
     justifyContent: 'center',
     borderRadius: 8,
     gap: 16,
@@ -210,13 +206,6 @@ const styles = StyleSheet.create({
   },
   stars: {
     flexDirection: 'row',
-  },
-  reviewScroll: {
-    flexGrow: 0,
-  },
-  sliderPagination: {
-    flexDirection: 'row',
-    gap: 16,
   },
   paginationText: {
     fontSize: 32,
